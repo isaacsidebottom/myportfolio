47ac8534cae1e9a17c20404ed2c3cca9e55b14c1
sideboisaa <sideboisaa@ecs.vuw.ac.nz>
2018-12-23 10:48:53 +1300

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio-web - Angular WITH GitHub Pages';
+  title = 'Angular LOOK AT ME GO';
 }
